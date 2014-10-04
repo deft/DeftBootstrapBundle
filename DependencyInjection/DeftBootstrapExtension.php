a0752007ee31050a2d6e3a7df20a0af49a50a17a
Jan Kramer <jan@deft.nl>
2014-10-04 09:40:16 +0200

    Cleanup to start rework for version 0.3

--- DependencyInjection/DeftBootstrapExtension.php
+++ DependencyInjection/DeftBootstrapExtension.php
@@ -7,23 +7,11 @@ use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class DeftBootstrapExtension extends Extension implements PrependExtensionInterface
+class DeftBootstrapExtension extends Extension
 {
     public function load(array $config, ContainerBuilder $container)
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        $bundles = $container->getParameter('kernel.bundles');
-        if (!isset($bundles['SpBowerBundle'])) {
-            return;
-        }
-
-        $container->prependExtensionConfig('sp_bower', [
-            'bundles' => ['DeftBootstrapBundle' => ['json_file' => 'bower.json']]
-        ]);
     }
 }
